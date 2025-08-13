d69062d27e947471589b79c17279d15cc1c62cb7
RodrigoFaria84 <rodrigofsilva84@outlook.com>
2025-08-13 16:43:24 -0300

    first commit

---
+++ src/main/java/com/br/cotiinformatica/configurations/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.br.cotiinformatica.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+			.title("Pedidos API - Treinamento Java Avançado: Formação Arquiteto")
+			.version("v1")
+			.description("Documentação da API do projeto")
+			.contact(new Contact()
+				.name("COTI Informática")
+				.email("contato@cotiinformatica.com.br")));
+	}
+
+}
